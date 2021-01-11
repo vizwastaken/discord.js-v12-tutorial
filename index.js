c23e2e48c747f58da58c0d8474d505c73792ca6c
vizwastaken <70798582+vizwastaken@users.noreply.github.com>
2021-01-11 21:49:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,22 @@ client.on('ready', () => {
    console.log(`${client.user.username} Successfully Logged in!`)
 })
 
+//we will make the system first otherwise it wont works
+client.snipes = new Map();
+client.on('messageDelete', function(message, channel) {
+      if (message.author.bot) return;
+      client.snipes.set(message.channel.id, {
+         content: message.content,
+         profilephoto: message.author.displayAvatarURL({ dynamic : true }),
+         author: message.author.tag,
+         date: message.createdTimestamp,
+         image: message.attachments.first() ? message.attachments.first().proxyURL : null
+      }) // that should be done...
+      // now let make utility folder and snipe files
+})
+
 module.exports = client;
-const { GiveawaysManager } = require('discord-giveaways');
+const { GiveawaysManager } = require('discord-giveaways'); //npm i discord-giveaways
 client.giveaways = new GiveawaysManager(client, {
    storage: './give.json',
    updateCountdownEvery: 5000, // 5000 in seconds is 5 seconds
@@ -36,7 +50,7 @@ client.on('message', async message => {
 
      const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
      if (message.content.match(prefixMention)) {
-     return message.channel.send(`Hii! <@${message.author.id}>, My prefix is \`${prefix}\``);
+     return message.channel.send(`Hii! <@${message.author.id}>, My prefix is \`${prefix}\``); //if you dont know what prefix of your bot, just ping it!
   }
      if(message.author.bot || message.channel.type === "dm") return;
      if (!message.content.startsWith(prefix)) return;
